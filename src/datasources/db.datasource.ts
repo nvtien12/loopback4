f5b377a1de12b1e51767e1e248146b08d80941cb
Hoc Lap Trinh <vtmbmt11231111@gmail.com>
2023-07-19 19:33:22 +0700

    first commit

--- src/datasources/db.datasource.ts
+++ src/datasources/db.datasource.ts
@@ -2,23 +2,24 @@ import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 
 const config = {
-  name: 'db',
-  connector: 'mysql',
+  name: process.env.DB_NAME,
+  connector: process.env.DB_CONNECTOR,
   url: '',
-  host: 'demo.cy2yumejupyy.ap-southeast-1.rds.amazonaws.com',
-  port: 3306,
-  user: 'admin',
-  password: 'Password',
-  database: 'product-keyshop'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 };
-
 // Observe application's life cycle to disconnect the datasource when
 // application is stopped. This allows the application to be shut down
 // gracefully. The `stop()` method is inherited from `juggler.DataSource`.
 // Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
 @lifeCycleObserver('datasource')
-export class DbDataSource extends juggler.DataSource
-  implements LifeCycleObserver {
+export class DbDataSource
+  extends juggler.DataSource
+  implements LifeCycleObserver
+{
   static dataSourceName = 'db';
   static readonly defaultConfig = config;
 
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
+import 'dotenv/config';
 import {ApplicationConfig, TodoListApplication} from './application';
-
 export * from './application';
 
 export async function main(options: ApplicationConfig = {}) {
   const app = new TodoListApplication(options);
   await app.boot();
   await app.start();
-
   const url = app.restServer.url;
   console.log(`Server is running at ${url}`);
   console.log(`Try ${url}/ping`);
